a94ad9e3582a7ee85eee1efabe48b92aecb0d9cc
MiltonCoronado <miltonext@hotmail.com>
2026-06-02 01:10:54 -0500

    Docs: a new coment adedd

--- src/gifs/api/giphy.api.ts
+++ src/gifs/api/giphy.api.ts
@@ -2,8 +2,11 @@ import axios from 'axios';
 
 //Instancia de axios con una URL base 'https://api.giphy.com/v1/gifs' cortada justo antes del "/search?"
 const giphyApi = axios.create({
+  //despues de "/gifs" ya podriamos tener el "/search" ó el "?"(query) osea mandar a llamar a cualquier end-point.
+  //ya que esta es la URL Base:
   baseURL: 'https://api.giphy.com/v1/gifs',
   params: {
+    //estos son parametros en comun por eso estos params van aca, compartido para toda la peticion Get.
     lang: 'es',
     api_key: import.meta.env.VITE_GIPHY_API_KEY,
   },
