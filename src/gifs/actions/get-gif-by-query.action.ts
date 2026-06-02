e3a359c1818aa97e797eeaf84d3bfc8a8ae4f7b0
MiltonCoronado <miltonext@hotmail.com>
2026-06-02 01:02:53 -0500

    test: add try/catch handling and spy on console.error for failed API requests

--- src/gifs/actions/get-gif-by-query.action.ts
+++ src/gifs/actions/get-gif-by-query.action.ts
@@ -4,23 +4,31 @@ import type { GiphyResponse } from '../interfaces/giphy.response';
 import type { Gif } from '../interfaces/gif.interface';
 
 const getGifsByQuery = async (query: string): Promise<Gif[]> => {
-  //Axios ó su Instancia si acepta genericos, no como fetch()!!!
-  const response = await giphyApi<GiphyResponse>('/search', {
-    //object configuration.
-    params: {
-      q: query,
-      limit: 10,
-    },
-  });
+  try {
+    if (query.trim().length === 0) {
+      return [];
+    }
+    //Axios ó su Instancia si acepta genericos, no como fetch()!!!
+    const response = await giphyApi<GiphyResponse>('/search', {
+      //object configuration.
+      params: {
+        q: query,
+        limit: 10,
+      },
+    });
 
-  //Patron mapper (mapear la data): tranformar una data que luce de una forma a tranformarla en otra(siendoLaMismasData)
-  return response.data.data.map((gif) => ({
-    id: gif.id,
-    title: gif.title,
-    url: gif.images.original.url,
-    width: Number(gif.images.original.width),
-    height: Number(gif.images.original.height), //con la Fn. constructora Number(), trasforma un string a numero.
-  }));
+    //Patron mapper (mapear la data): tranformar una data que luce de una forma a tranformarla en otra(siendoLaMismasData)
+    return response.data.data.map((gif) => ({
+      id: gif.id,
+      title: gif.title,
+      url: gif.images.original.url,
+      width: Number(gif.images.original.width),
+      height: Number(gif.images.original.height), //con la Fn. constructora Number(), trasforma un string a numero.
+    }));
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
 };
 
 export { getGifsByQuery };
