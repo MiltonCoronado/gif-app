bf587df32818e0f12896baaedab2b4e32cf2b96e
MiltonCoronado <miltonext1@gmail.com>
2026-07-07 23:34:41 -0500

    chore: comments was deleted

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,8 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
-// import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-// import babel from '@rolldown/plugin-babel'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    // babel({ presets: [reactCompilerPreset()] })
-  ],
+  plugins: [react()],
   test: {
     environment: 'jsdom',
     globals: true,
